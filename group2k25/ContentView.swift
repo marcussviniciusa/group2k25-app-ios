15809a1da6e5b73dbd39eda38ea5191e879aabad
marcussviniciusa <marcussviniciusa@gmail.com>
2026-02-08 17:41:49 -0300

    Initial Commit

---
+++ group2k25/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  group2k25
+//
+//  Created by Marcus Vinicius Araujo Mendonca Trindade on 08/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ group2k25/group2k25App.swift
@@ -0,0 +1,17 @@
+//
+//  group2k25App.swift
+//  group2k25
+//
+//  Created by Marcus Vinicius Araujo Mendonca Trindade on 08/02/26.
+//
+
+import SwiftUI
+
+@main
+struct group2k25App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
